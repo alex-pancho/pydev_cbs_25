257eb3dca0a763e550a8693a0f5f8d77df4e27dd
Dmitro <dmitry234230@gmail.com>
2025-10-19 18:05:26 +0300

    finished doing homework

--- lesson_201/homework.py
+++ lesson_201/homework.py
@@ -13,3 +13,39 @@
 Створіть клас, який описує відгук до книги. Додайте до класу книги поле(атрибут) – список_відгуків. Зробіть так,
 щоб при виведенні обєкту классу книги на екран за допомогою функції print (__str__) також виводилися відгуки до неї.
 """
+# Вирішив переробити 0 завдання, щоб попрактикувати
+class Student():
+    def __init__(self):
+        self.name = "Іван"
+        self.age = 23
+        self.grade = 6
+    def changegrade(self, newgrade):
+        self.grade = newgrade
+
+study = Student()
+study.changegrade(newgrade=10)
+print(study.name, study.age, study.grade)
+
+class Book():
+    def __init__(self, year, author, genre, book):
+        self.author = author
+        self.year = year
+        self.genre = genre
+        self.book = book
+    def __str__(self):
+        return f'Назва книги: {self.book} Автор: {self.author} Рік видання: {self.year} Жанр: {self.year}'
+infobook = Book("Гамлет", "Вільям Шекспір", 1623, "Трагедія")
+print(infobook)
+
+class Review():
+    def __init__(self):
+        self.user = input("Напишіть ваше ім'я: ")
+        self.rating = int(input("Напишіть оцінку до книги: "))
+        self.comment = input("Напишіть коментар до книги: ")
+    def __str__(self):
+        return f'Користувач: {self.user} Оцінка до книги: {self.rating} Коментар: {self.comment}'
+review1 = Review()
+print(review1)
+# Вирішив попрактикувати з input щоб зробити відгуки більш функціональними
+    
+        
