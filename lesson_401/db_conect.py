b6adfe78f845afe66600cf17685133302c3a4edf
Oleksandr Panchenko <pan.pan.4it@gmail.com>
2025-12-16 20:55:49 +0200

    ls 401

---
+++ lesson_401/db_conect.py
@@ -0,0 +1,31 @@
+import psycopg2
+from psycopg2 import OperationalError
+
+def create_connection(db_name, db_user, db_password, db_host, db_port):
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            database=db_name,
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port,
+        )
+        print("Connection to PostgreSQL DB successful")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+    return connection
+
+database="norwind"
+user="postgres"
+password="pan"
+host="127.0.0.1"
+port=5432
+
+connection = create_connection(
+    db_name=database,
+    db_user=user,
+    db_password=password,
+    db_host=host,
+    db_port=port
+)
